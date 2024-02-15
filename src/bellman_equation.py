96b4cf96dd671ea22cfc3d3cdf8c277ef9fa54df
Tristan Tran <64502081+tristntran@users.noreply.github.com>
2024-02-15 09:59:10 -0800

    modifications for class

--- src/bellman_equation.py
+++ src/bellman_equation.py
@@ -1,16 +1,23 @@
 """
 Creating general functions to solve dynamic programs
 """
-from typing import List, Tuple
+from typing import List, NamedTuple
 import numpy as np
-from collections import namedtuple
 
-WEIGHTS = np.array([1,2,3,4,5,6,7])
-VALUES = np.array([7, 5, 2, 8, 7, 9, 4])
+WEIGHTS = np.arange(1,8)
+VALUES = np.arange(7,0,-1)
 
-knapsackState = namedtuple('knapsackState', ['remaining_weight', 'item_idx'])
+class KnapsackState(NamedTuple):
+    """Type of named tuple that contains two attributes,
+    remaining weight and item_idx
 
-def actions(state: knapsackState)->List:
+    Args:
+        NamedTuple (_type_): _description_
+    """
+    remaining_weight: int
+    item_idx: int = 0
+
+def actions(state: KnapsackState)->List:
     """returns a list of actions given our current state
 
     Args:
@@ -19,9 +26,15 @@ def actions(state: knapsackState)->List:
     Returns:
         List: returns a list 0 and 1. 0 for leave. 1 for take
     """
+    if state.item_idx == WEIGHTS.shape[0] - 1:
+    # if it is the last item no more actions can be taken
+        return []
+    if state.remaining_weight < WEIGHTS[state.item_idx]:
+    # if we exceed our weight capacity we can only leave the item
+        return [0]
     return [0, 1]
 
-def transition(state: knapsackState, action: int):
+def transition(state: KnapsackState, action: int):
     """takes in our current state and our choice of action and returns a new state
 
     Args:
@@ -29,12 +42,12 @@ def transition(state: knapsackState, action: int):
         action (_type_): _description_
     """
     if action ==1: # we take the object
-        return knapsackState(state.remaining_weight - WEIGHTS[state.item_idx],
+        return KnapsackState(state.remaining_weight - WEIGHTS[state.item_idx],
                              state.item_idx + 1)
     else: # do not take object
-        return knapsackState(state.remaining_weight, state.item_idx + 1)
+        return KnapsackState(state.remaining_weight, state.item_idx + 1)
 
-def immediate_reward(state:knapsackState, action)->float:
+def immediate_reward(state:KnapsackState, action)->float:
     """gives us our immediate payoff
 
     Args:
@@ -44,9 +57,11 @@ def immediate_reward(state:knapsackState, action)->float:
     Returns:
         float: _description_
     """
-    ...
+    if action:
+        return VALUES[state.item_idx]
+    return 0
 
-def bellman_equation(state: knapsackState)-> float:
+def bellman_equation(state: KnapsackState)-> float:
     """solves dynamic programming
 
     Args:
@@ -55,10 +70,13 @@ def bellman_equation(state: knapsackState)-> float:
     Returns:
         float: _description_
     """
-    max_reward = -np.inf
+    # we set to 0 instead of - infinity because all values are guaranteed
+    # positive by problem definition
+    max_reward = 0 
     for a in actions(state):
         new_state = transition(state, a)
         future_reward = bellman_equation(new_state)
         reward = immediate_reward(state, a) + future_reward
         if reward > max_reward:
             max_reward = reward
+    return max_reward
--- tests/test_bellman_equation.py
+++ tests/test_bellman_equation.py
@@ -1,14 +1,39 @@
 """
 writes one test per function of bellman equation
 """
-from bellman_equation import knapsackState, actions, transition, immediate_reward, bellman_equation
+import pytest
+from bellman_equation import (KnapsackState, actions, transition, 
+                              immediate_reward, bellman_equation,
+                              WEIGHTS, VALUES)
 
 def test_actions():
     """
     funcitonal test for the actions function
     """
-    test_state = knapsackState(0,0)
+    test_state = KnapsackState(0,0)
     assert actions(test_state) == [0,1]
 
 def test_transition():
     assert True
+
+def test_immediate_reward():
+    assert True
+
+def test_bellman_equation():
+    start_state = KnapsackState(1,0)
+    assert bellman_equation(start_state) == 7
+    start_state = KnapsackState(2,0)
+    assert bellman_equation(start_state) == 7
+    start_state = KnapsackState(3,0)
+    assert bellman_equation(start_state) == 13
+
+@pytest.mark.parametrize("state, expected", [
+    (KnapsackState(1,0), 7),
+    (KnapsackState(2,0), 7),
+    (KnapsackState(3,0), 13)
+])
+def test_bellman_equation_parameterized(state, expected):
+    """
+    better version
+    """
+    assert bellman_equation(state) == expected
